6116a9521bba9a902bb384a9b827169cbe800062
Sideral TI <sideralti.app@gmail.com>
2025-01-27 09:24:12 -0300

    initial project

---
+++ src/main/java/com/sideralti/app/SoftwareSpringNapolisDeliPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringNapolisDeliPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSpringNapolisDeliPizzaApplication.class, args);
+	}
+
+}
